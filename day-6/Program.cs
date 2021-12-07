eee9e4fa3b09dcd0e85a0d17c2df9246dd650325
Claudio <the_wolf91@libero.it>
2021-12-07 18:23:49 +0100

    Day 6 part 2.....

--- day-6/Program.cs
+++ day-6/Program.cs
@@ -30,7 +30,7 @@ namespace day_6
                     else if (numbers[i] == 0)
                     {
                         numbers[i] = 6;
-                        numbers.Add(8+1);
+                        numbers.Add(8 + 1);
                     }
                 }
 
@@ -38,12 +38,35 @@ namespace day_6
             }
 
             return numbers.Count;
-
         }
 
-        static int PartTwo()
+        static long PartTwo()
         {
-            return 0;
+            List<int> numbers = input[0].Split(',').Select(int.Parse).ToList();
+            Dictionary<int, long> fish = new Dictionary<int, long>
+                {{0, 0}, {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0}, {8, 0}};
+
+            int days = 256;
+
+            foreach (var n in numbers)
+            {
+                fish[n] += 1;
+            }
+
+            while (days > 0)
+            {
+                long fishZero = fish[0];
+                for (int i = 0; i < fish.Count - 1; i++)
+                {
+                    fish[i] = fish[i + 1];
+                }
+
+                fish[8] = fishZero;
+                fish[6] += fishZero;
+                days--;
+            }
+
+            return fish.Select(x => x.Value).Sum();
         }
     }
 }
