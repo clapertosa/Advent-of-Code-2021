7afdb96198bac6c9008120d4c1a67f839857f331
Claudio <lapertosa.c@gmail.com>
2021-12-11 17:37:06 +0100

    Day 7 part two.

---
+++ day-7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_7
+{
+    class Program
+    {
+        private static string[] input;
+
+        static void Main(string[] args)
+        {
+            input = File.ReadAllLines("./input.txt");
+            Console.WriteLine(PartOne());
+            Console.WriteLine(PartTwo());
+        }
+
+
+        static int PartOne()
+        {
+            int[] parse = input[0].Split(',').Select(int.Parse).ToArray();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < parse.Max(); i++)
+            {
+                numbers.Add(0);
+                foreach (var n in parse)
+                {
+                    numbers[i] += Math.Abs(i - n);
+                }
+            }
+
+            return numbers.Min();
+        }
+
+        static int PartTwo()
+        {
+            int[] parse = input[0].Split(',').Select(int.Parse).ToArray();
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < parse.Max(); i++)
+            {
+                numbers.Add(0);
+                foreach (var n in parse)
+                {
+                    int steps = 0;
+                    for (int j = 0; j < Math.Abs(n - i); j++)
+                    {
+                        steps += j + 1;
+                    }
+
+                    numbers[i] += steps;
+                }
+            }
+
+            return numbers.Min();
+        }
+    }
+}
